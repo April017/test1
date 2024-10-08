cb290701bfb86a1733af6d24c5bed28406c13d1d
April017 <aprilfaithlibrero017@gmail.com>
2024-10-08 12:51:48 +0800

    commit

---
+++ App/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+class HomePageview(TemplateView):
+    template_name = 'home.html'
+
+class AboutPageview(TemplateView):
+    template_name = 'about.html'
